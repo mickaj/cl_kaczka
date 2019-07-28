0bb6802d39ed69bf6737fdbbf903ff55c69ba45a
Mic Kajzer <mickaj@mkajzer.pl>
2019-07-28 20:04:28 +0200

    js files added

---
+++ js/game.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+    moveDuck();
+}
+
+function moveDuck() {
+    var duckDiv = $('#duck');
+    var currentX = duckDiv.offsetLeft;
+    var currentY = duckDiv.offsetTop;
+
+    var timeout = setInterval(function () {
+        duckDiv.animate({
+            'left': "+=10px",
+            'top': "+=10px"
+        }, 10);
+    }, 15);
+}
